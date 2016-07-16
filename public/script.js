4667fc0711ddd7d86e3991da83108969cd9cd3d1
Zachary Smith <william.smith1@wipro.com>
2016-07-16 11:21:53 -0400

    Add another pair of spots
    Two smaller spots at the top. Size, position, and color are randomized, within specified ranges.

--- public/script.js
+++ public/script.js
@@ -21,9 +21,25 @@ var color = randomColor()
 var beetle = document.getElementsByClassName('beetle')[0]
 beetle.style.backgroundColor = color
 
-color = randomColor()
-var spots = document.getElementsByClassName('spot')
+var topRow = document.getElementsByClassName('row')[0]
+topRow.style.paddingTop = '35px'
+var bottomRow = document.getElementsByClassName('row')[1]
+bottomRow.style.paddingTop = '35px'
 
+color = randomColor()
+var spots = topRow.children
+var spotSize = random(20, 50) + 'px'
+for (var i=0; i < spots.length; i++) {
+  var spot = spots.item(i)
+  spot.style.backgroundColor = color
+  spot.style.width = spotSize
+  spot.style.height = spotSize
+  spot.style.marginTop = random(10, 60) + 'px'
+
+}
+
+color = randomColor()
+spots = bottomRow.children
 for (var i=0; i < spots.length; i++) {
   spots.item(i).style.backgroundColor = color
 }
