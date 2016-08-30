87caecc6bbc941f9b048d85437e4a6c0b624a67c
Zachary Smith <william.smith1@wipro.com>
2016-08-30 19:58:23 -0400

    Add makeTriangle function

---
+++ src/createTriangle.js
@@ -0,0 +1,16 @@
+module.exports = options => {
+
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  const { x, y, color, size } = options
+  const halfWidth = size / 2
+
+  path.setAttribute('d',
+    `M ${x}             ${y}
+     L ${x - halfWidth} ${y + size}
+     L ${x + halfWidth} ${y + size}
+     L ${x}             ${y}`)
+  path.setAttribute('fill', color)
+  path.setAttribute('opacity', 0.5)
+
+  return path
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 require('./style.css')
 const createSpot = require('./createSpot')
+const createTriangle = require('./createTriangle')
 
 const container = document.getElementById('container')
 
 const spot = createSpot({
-  color: 'tomato',
   x: 300,
   y: 300,
+  color: 'tomato',
 })
 
+
+const tri = createTriangle({
+  x: 300,
+  y: 235,
+  color: 'aqua',
+  size: 100,
+})
+
+container.appendChild(tri)
 container.appendChild(spot)
