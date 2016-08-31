06242579911211697a2831fdac6dbe584e7afca4
Zachary Smith <william.smith1@wipro.com>
2016-08-30 20:02:44 -0400

    Add size option to circle

--- src/createSpot.js
+++ src/createSpot.js
@@ -1,9 +1,12 @@
 module.exports = options => {
   const spot = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+  const { x, y, size, color } = options
+
   spot.setAttribute('cx', options.x)
   spot.setAttribute('cy', options.y)
-  spot.setAttribute('r', 20)
+  spot.setAttribute('r', size)
   spot.setAttribute('fill', options.color)
   spot.setAttribute('opacity', 0.5)
+
   return spot
 }
--- src/main.js
+++ src/main.js
@@ -4,19 +4,27 @@ const createTriangle = require('./createTriangle')
 
 const container = document.getElementById('container')
 
+const body = createSpot({
+  x: 200,
+  y: 200,
+  size: 150,
+  color: 'gray',
+})
+
 const spot = createSpot({
-  x: 300,
-  y: 300,
+  x: 200,
+  y: 200,
+  size: 20,
   color: 'tomato',
 })
 
-
 const tri = createTriangle({
-  x: 300,
-  y: 235,
+  x: 200,
+  y: 185,
   color: 'aqua',
   size: 100,
 })
 
+container.appendChild(body)
 container.appendChild(tri)
 container.appendChild(spot)
