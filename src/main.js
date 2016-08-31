04d17b55955b5d39efdd8f694f8bd6c530061ec5
Zachary Smith <william.smith1@wipro.com>
2016-08-30 20:04:43 -0400

    Use random colors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 require('./style.css')
 const createSpot = require('./createSpot')
 const createTriangle = require('./createTriangle')
+var randomColor = require('./randomColor')
 
 const container = document.getElementById('container')
 
@@ -8,20 +9,20 @@ const body = createSpot({
   x: 200,
   y: 200,
   size: 150,
-  color: 'gray',
+  color: randomColor(40),
 })
 
 const spot = createSpot({
   x: 200,
   y: 200,
   size: 20,
-  color: 'tomato',
+  color: randomColor(40),
 })
 
 const tri = createTriangle({
   x: 200,
   y: 185,
-  color: 'aqua',
+  color: randomColor(40),
   size: 100,
 })
 
